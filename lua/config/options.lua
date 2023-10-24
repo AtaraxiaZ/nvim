f478529121770c88f253b6a9f7345cd8f6634c56
李哲 <zheli@bytedance.com>
2023-10-24 15:34:39 +0800

    minor revision

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -54,6 +54,7 @@ local options = {
   scrolloff = 8, -- is one of my fav
   sidescrolloff = 8,
   guifont = "monospace:h17", -- the font used in graphical neovim applications
+  tags = "./tags,tags;/",
 }
 
 vim.opt.shortmess:append("c")
