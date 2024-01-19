3edc28bebe485f7c145d74a6eb40b0450be1fa05
Zhe Li <ataraxialex@gmail.com>
2024-01-19 10:23:30 +0800

    add vscode shortcut

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 if vim.g.vscode then
+  vim.cmd([[nnoremap <silent> gr <cmd>lua require('vscode-neovim').call('editor.action.referenceSearch.trigger')<cr>]])
+  vim.cmd(
+    [[nnoremap <silent> <s-h> <cmd>lua require('vscode-neovim').call('workbench.action.previousEditorInGroup')<cr>]]
+  )
+  vim.cmd([[nnoremap <silent> <s-l> <cmd>lua require('vscode-neovim').call('workbench.action.nextEditorInGroup')<cr>]])
 else
   -- It may make a mass in vscode
   require("config.lazy")
